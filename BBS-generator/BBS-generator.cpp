a8f93c6daf00631d4130e9da37c16c9b31bb6abf
pogodagreen <pogodagreen@gmail.com>
2019-10-28 07:09:35 +0100

    implemented isPrime & isCoPrime funs

--- BBS-generator/BBS-generator.cpp
+++ BBS-generator/BBS-generator.cpp
@@ -1,21 +1,46 @@
-// BBS-generator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <math.h>
+
+bool isPrime(uint64_t x)
+{
+	for (int i = 2; i < (double)sqrt(x); i++)
+	{
+		if (x%i == 0)
+		{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
+uint64_t NWD(uint64_t x, uint64_t y)
+{
+	uint64_t pom;
+	while (y != 0)
+	{
+		pom = y;
+		y = x % y;
+		x = pom;
+	}
+	return x;
+}
+
+bool isCoPrime(uint64_t N, uint64_t x)
+{
+	NWD(N, x) == 1;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	uint64_t p, q, N;
+	if (isPrime(p) && isPrime(q))
+	{
+		if (p % 4 == 3 && q % 4 == 3)
+		{
+			N = p * q;
+		}
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
