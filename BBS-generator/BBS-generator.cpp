e9c575276fd59dcb8327c501425e75520d6211b1
pogodagreen <pogodagreen@gmail.com>
2019-10-28 07:55:30 +0100

    generate sequence

--- BBS-generator/BBS-generator.cpp
+++ BBS-generator/BBS-generator.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include <iostream>
 #include <math.h>
+#include <string>
+
+using namespace std;
 
 bool isPrime(uint64_t x)
 {
@@ -8,11 +11,11 @@ bool isPrime(uint64_t x)
 	{
 		if (x%i == 0)
 		{
-			return true;
+			return false;
 			break;
 		}
 	}
-	return false;
+	return true;
 }
 
 uint64_t NWD(uint64_t x, uint64_t y)
@@ -29,18 +32,52 @@ uint64_t NWD(uint64_t x, uint64_t y)
 
 bool isCoPrime(uint64_t N, uint64_t x)
 {
-	NWD(N, x) == 1;
+	if (NWD(N, x) == 1)
+		return true;
+	else
+		return false;
 }
 
-int main()
+string generateSequence(uint64_t x, uint64_t N, int length)
 {
-	uint64_t p, q, N;
+	string klucz;
+	string pom = "0";
+	int value = (x*x) % N;
+	for (int i = 0; i < length; i++)
+	{
+		pom=to_string(value & 1);
+		klucz.append(pom);
+		value = (value*value) % N;
+		
+	}
+	return klucz;
+}
+
+string BBS(uint64_t p, uint64_t q, uint64_t x, int length)
+{
+	uint64_t N;
 	if (isPrime(p) && isPrime(q))
 	{
 		if (p % 4 == 3 && q % 4 == 3)
 		{
 			N = p * q;
+			if (isCoPrime(N, x))
+			{
+				return generateSequence(x, N, length);
+
+			}
 		}
 	}
+	else
+		return "Blad";
+}
+
+int main()
+{
+	uint64_t p=11, q=19, x=281;
+	int length=20000;
+	string sequence = BBS(p, q, x, length);
+	cout << sequence;
+	getchar;
 }
 
